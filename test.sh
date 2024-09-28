b381f381d0f9346a78a6da9c14d106e2d30f34b1
Doug Horner <horner@mieweb.com>
2024-09-28 00:50:10 -0400

    Update test.sh with docs

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# This is the main script
+
 ./makekeys.sh
 docker build -t sshd-ldap .
+
+# I like to have this inline:  that way I kill and restart the docker in one bash history.
+# the rm kills and removes the old instance, and the docker runs a new one.
+# -n is a good way to name a contaier and kill it quickly. 
 docker rm -f sshd-ldap; docker run -d --name sshd-ldap -p 2222:2222 -e HOSTNAME=doug -e DOMAIN_NAME=mieweb.com sshd-ldap
 
-# test the ssh connection and accpet the fingerprint
+# test the ssh connection and accpet the fingerprint (only works if you are in the project folder)
 ssh -p 2222 -i id_rsa mie@localhost
